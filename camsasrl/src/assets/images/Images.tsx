9d8b2258a5c2d22de1255ab96546bd6655df9887
Cristian Sombra <cristiansombra87@gmail.com>
2024-09-17 05:42:15 -0300

    Styles update

--- camsasrl/src/assets/images/Images.tsx
+++ camsasrl/src/assets/images/Images.tsx
@@ -1,2 +1,3 @@
 export const logoNav = 'https://res.cloudinary.com/dyh8yawnl/image/upload/v1726035903/Logo1_1_xlzue9.png';
 export const imageHome = 'https://res.cloudinary.com/dyh8yawnl/image/upload/v1726525518/Home_nfkll8.webp';
+export const imageHomeMovile = 'https://res.cloudinary.com/dyh8yawnl/image/upload/v1726562480/Home_movile_vtf64f.webp';
