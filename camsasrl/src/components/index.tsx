9c0a1be69f40b909df9e72b1d0e2865d075c2783
CristianLegion <108194989+CristianSombra@users.noreply.github.com>
2024-09-19 20:24:19 -0300

    ScrollToTop and FormContact create.

--- camsasrl/src/components/index.tsx
+++ camsasrl/src/components/index.tsx
@@ -2,8 +2,10 @@ import AnimateProps from "./props/animateSection/animateSection";
 import ButtonSeeMore from "./buttonSeeMore/ButtonSeeMore";
 import { SpinnerLoader } from "./spinnerLoader/SpinnerLoader";
 import FormContact from "./formContact/formContact";
+import { ScrollToTop } from "./scrollToTop/ScrollToTop";
 
 export type { AnimateProps };
 export { ButtonSeeMore };
 export { SpinnerLoader };
 export { FormContact };
+export { ScrollToTop };
