afd28ebdf0c160bc7d20a0015ce0bb41d2386047
CristianLegion <108194989+CristianSombra@users.noreply.github.com>
2024-09-19 19:51:40 -0300

    Contact update

--- camsasrl/src/pages/contact/Contact.tsx
+++ camsasrl/src/pages/contact/Contact.tsx
@@ -1,9 +1,12 @@
 import React from "react";
 import { motion } from "framer-motion";
-import { sectionAbout } from "../../assets/images/Images";
 import { AnimateProps } from "../../components";
+import { FormContact } from "../../components/index";
+import "../../styles";
 
 const Contact: React.FC <AnimateProps>= ({animate}) => {
+    const handleFormSuccess = () => {}
+
     return (
         <motion.section
             initial={{opacity: 0}}
@@ -21,28 +24,32 @@ const Contact: React.FC <AnimateProps>= ({animate}) => {
                         animate={ animate ? {opacity:1} : {opacity:0}}
                         transition={{duration: 0.3, delay: 0.4}}
                         >
-                            BIENVENIDO A CAMSA
+                            PUEDES ESCRIBIRNOS
                     </motion.h1>
                     <motion.h2
                         initial={{x: 10, opacity: 0}}
                         animate={ animate ? {x: 0, opacity:1} : {x: 10, opacity: 0}}
                         transition={{duration: 0.4, delay: 0.4}}
                         >
-                            Un lugar para recibir atención
+                            Contacto
                     </motion.h2>
                 </div>
             </div>
 
             {/* Section */}
-            <div className="row my-4 d-flex justify-content-center">
-                <div className="col-12 col-md-8 text-center">
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur asperiores amet praesentium harum nam unde placeat id earum libero minus ea iste atque, autem quibusdam qui dolore. Ullam, ab aperiam!</p>
+            <div className="row mb-5 d-flex justify-content-center">
+                <div className="col-12 col-md-8 col-lg-6">
+                    <FormContact onSuccess={handleFormSuccess} />
                 </div>
             </div>
 
-            <div className="row d-flex justify-content-center">
-                <div className="col-12 col-md-8">
-                    <img src={sectionAbout} className="img-fluid rounded shadow-image" alt="" />
+
+            <div className="row mb-5 d-flex justify-content-center text-center">
+                <div className="col-12 col-md-8 mb-5">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque molestias, eum reiciendis voluptatem dolorem sint excepturi eligendi aliquid consectetur corporis facere sequi quasi animi, assumenda dolor nulla. Eveniet, laboriosam quasi quae accusamus in aperiam sed provident ipsum architecto eligendi ab incidunt tempore aut totam iusto, alias iure vero et maiores.</p>
+                </div>
+                <div className="col-12 col-md-8 mb-5">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque molestias, eum reiciendis voluptatem dolorem sint excepturi eligendi aliquid consectetur corporis facere sequi quasi animi, assumenda dolor nulla. Eveniet, laboriosam quasi quae accusamus in aperiam sed provident ipsum architecto eligendi ab incidunt tempore aut totam iusto, alias iure vero et maiores.</p>
                 </div>
             </div>
             
--- camsasrl/src/styles/components/index.tsx
+++ camsasrl/src/styles/components/index.tsx
@@ -3,4 +3,5 @@ import './buttons/buttonTitle/ButtonTitle.css';
 import './buttons/buttonPrimary/ButtonPrimary.css';
 import './buttons/buttonSecondary/ButtonSecondary.css';
 import './buttons/buttonThirdy/ButtonThirdy.css';
+import './formContact/Form.css';
 import './spinnerLoader/SpinnerLoader.css';
