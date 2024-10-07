3a0332652218c7180544f0ce109e6a848f62a65a
Cristian Sombra <cristiansombra87@gmail.com>
2024-10-07 05:08:08 -0300

    Style update.

--- camsasrl/src/layouts/navbar/Navbar.tsx
+++ camsasrl/src/layouts/navbar/Navbar.tsx
@@ -6,9 +6,7 @@ import { faWhatsapp } from '@fortawesome/free-brands-svg-icons';
 
 const Navbar: React.FC = React.memo(() => {
     return (
-        <nav
-            className="navbar navbar-light bg-white fixed-top"
-        >
+        <nav className="navbar navbar-light bg-white fixed-top d-none d-md-block">
             <div className="container-fluid d-flex justify-content-between align-items-center">
                 <div className="navbar-brand">
                     <img src={logo} alt="Logo" className="img-fluid" style={{ width: '260px' }} />
@@ -17,7 +15,7 @@ const Navbar: React.FC = React.memo(() => {
                     initial={{ opacity: 0 }}
                     animate={{ opacity: 1 }}
                     transition={{ duration: 0.6, delay: 0.8 }}
-                    className="navbar-nav d-none d-lg-flex flex-row ms-auto">
+                    className="navbar-nav d-lg-flex flex-row ms-auto">
                         <li className="nav-item mx-3">
                             <a href="#Services" className="nav-link" aria-label="Servicios">Servicios</a>
                         </li>
